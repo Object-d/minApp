d34e6e9ec4938f0ece379e2de2e82ef62d33ca51
jlihua <1198747340@qq.com>
2023-06-01 08:01:03 +0800

    feat: agree privacy

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,34 @@
 <template>
   <view class="index">
-    <img :src="loginSvg">
-    <button class="btn-max-w btn" type="primary" @click="onEnter">申请入园</button>
-    <view class="btn-history" type="text" @click="onEnterHistory">预约历史</view>
+    <img :src="loginSvg" />
+    <button
+      :disabled="!agree"
+      class="btn-max-w btn"
+      type="primary"
+      @click="onEnter"
+    >
+      申请入园
+    </button>
+    <view class="index-agree">
+      <nut-checkbox v-model="state.agree" label="本人已阅读并同意">
+        本人已阅读并同意 <view class="index-privacy" @click="openUser">《用户隐私政策》</view>
+      </nut-checkbox>
+    </view>
+    <view class="btn-history" type="text" @click="onEnterHistory"
+      >预约历史</view
+    >
   </view>
 </template>
 
 <script setup>
 import Taro from "@tarojs/taro";
+import { computed, onMounted, reactive, ref } from "vue";
 
-const loginSvg = require('./../../assets/login.svg');
+const loginSvg = require("./../../assets/login.svg");
+
+const state = reactive({
+  agree: false,
+});
 
 function onEnter() {
   Taro.navigateTo({
@@ -23,6 +42,28 @@ function onEnterHistory() {
   });
 }
 
+function openUser(e) {
+  e.stopPropagation();
+  Taro.downloadFile({
+    url: "../../assets/privacy-policy.docx",
+    success: (res) => {
+      const filePath = res.tempFilePath;
+      Taro.openDocument({
+        filePath: filePath,
+        fileType: "docx",
+        success: () => {
+          console.log("打开文档成功");
+        },
+        fail: () => {
+          console.log("打开文档失败");
+        },
+      });
+    },
+    fail: () => {
+      console.log("下载文件失败");
+    },
+  });
+}
 </script>
 
 <style lang="scss">
@@ -33,6 +74,23 @@ function onEnterHistory() {
   align-items: center;
   box-sizing: border-box;
   padding-top: 15vh;
+
+  &-agree {
+    transform: scale(0.8);
+    font-size: 12px;
+    margin-top: 20px;
+  }
+
+  &-privacy {
+    display: inline;
+    color: #2196f3;
+  }
+
+  button.btn[disabled][type="primary"] {
+    opacity: 0.5;
+    background-color: #2196f3;
+    cursor: not-allowed;
+  }
   .btn {
     width: 70%;
     background-color: #2196f3;
